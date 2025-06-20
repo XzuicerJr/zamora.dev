a73e53855006c83ae57d9b75d066b651fc6d18f1
César Zamora <zuicerjr24@gmail.com>
2025-06-19 22:49:45 -0600

    fix: remove ngrok url

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
-  server: {
-    allowedHosts: ['ea35-2601-8c0-d01-2d50-e49c-66a8-ac8e-bbdc.ngrok-free.app']
-  }
+  plugins: [sveltekit()]
 });
